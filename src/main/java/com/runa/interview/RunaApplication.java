f04be3b5c460f5017a40f7ba36c5087873d8e62b
Yefri Velasquez <yefrivelasquez@gmail.com>
2024-11-22 15:08:42 +0000

    Updated

--- src/main/java/com/runa/interview/RunaApplication.java
+++ src/main/java/com/runa/interview/RunaApplication.java
@@ -1,7 +1,7 @@
 package com.runa.interview;
 
-public class main {
+public class RunaApplication {
     public static void main(String[] args) {
-
+        System.out.println("Hello, World!");
     }
 }
---
+++ src/test/java/RunaApplicationTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RunaApplicationTest {
+
+    @Test
+    void testRunaApplication() {
+        String expectedOutput = "Hello, World!";
+        String actualOutput = "Hello, World!";
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+}
